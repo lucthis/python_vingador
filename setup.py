f5c850078f75143e215d3c3425bde02c755ad803
Caio Ross <49957185+caioross@users.noreply.github.com>
2020-10-22 01:05:29 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='Python Vingador',
+      version='2.1.2',
+      description='Executor ligacao direto',
+      url='https://github.com/caioross/python_vingador',
+      author='Caio Ross',
+      author_email='kio199@gmail.com',
+      license='CCT-0',
+      packages=['pyyvingador'],
+      python_requires='>=3',
+      classifiers=['Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'],
+      zip_safe=False)
